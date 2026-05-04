bdffbb51a93eafeb2ceb5343659bc8e44fc030cf
verycube <henkka008@hotmail.com>
2026-05-04 03:00:39 +0300

    started working on split_nodes_delimitter but in progress

--- src/textnode.py
+++ src/textnode.py
@@ -10,7 +10,6 @@ class TextType(Enum):
     LINK = "link"
     IMAGE = "image"
 
-
 class TextNode():
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -25,10 +24,6 @@ class TextNode():
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
     
-    
-    
-
-
 def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.TEXT:
         return LeafNode(None, text_node.text)
@@ -43,3 +38,32 @@ def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.IMAGE:
         return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
     raise ValueError(f"invalid text type: {text_node.text_type}")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    from textnode import TextNode, TextType
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        # 1. If old_node is not a TEXT type, append it as-is and continue
+        if old_node is not text_type:
+            new_nodes.append(old_node)
+        elif old_node is text_type:
+            parts = old_node.split({delimiter})
+            if not len(parts) % 3:
+                raise Exception("Unmatched delimitter")
+        # 2. Otherwise, split old_node.text using the delimiter
+        #    e.g. parts = old_node.text.split(delimiter)
+        
+        # 3. If the number of parts is even, there's an unmatched delimiter
+        #    -> raise an exception
+        
+        # 4. Loop through the parts:
+        #    - even-indexed parts are plain TEXT
+        #    - odd-indexed parts are the special text_type
+        #    - skip empty strings if you like
+        #    - append each as a TextNode to a temporary list
+        
+        # 5. extend new_nodes with that temporary list
+        pass
+    return new_nodes
